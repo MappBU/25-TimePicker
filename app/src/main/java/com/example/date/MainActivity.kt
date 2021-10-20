a5548696ec85b8dc4d431f1f92831d1e11268ea1
leif2k <leif2k@yandex.ru>
2021-10-20 22:33:20 +0300

    Data Picker

--- app/src/main/java/com/example/date/MainActivity.kt
+++ app/src/main/java/com/example/date/MainActivity.kt
@@ -3,68 +3,68 @@ package com.example.date
 import android.annotation.SuppressLint
 import android.app.DatePickerDialog
 import android.app.Dialog
+import android.app.TimePickerDialog
 import android.content.DialogInterface
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
-import android.widget.DatePicker
+import android.widget.TimePicker
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.example.date.databinding.ActivityMainBinding
 
 // Интерфейс для Дата пикера
-class MainActivity : AppCompatActivity(),View.OnClickListener, DatePickerDialog.OnDateSetListener {
+class MainActivity : AppCompatActivity(),View.OnClickListener, TimePickerDialog.OnTimeSetListener {
 
     private var binding: ActivityMainBinding? = null
-    // Создаем объект Дата пикера
-    var datePickerDialog:DatePickerDialog? =null
-    // Получаем с календаря 3 переменные (int)
-    private var day = 0
-    private var month = 0
-    private var year = 0
+    // Создаем объект Тайм пикера
+    var timePickerDialog: TimePickerDialog? =null
+    // Получаем с ТАйм пикера 2 переменные (int)
+    private var hours = 0
+    private var minutes = 0
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        binding?.selectDate?.setOnClickListener(this)
+        binding?.selectTime?.setOnClickListener(this)
     }
 
-    // При нажатии на кнопку запускаем метод tartDataPicker
+    // При нажатии на кнопку запускаем метод startTimePicker
     override fun onClick(view: View) {
 
         when(view.id) {
-            R.id.selectDate -> {
-                startDataPicker()
+            R.id.selectTime -> {
+                startTimePicker()
             }
 
         }
     }
 
-    private fun startDataPicker () {
+    private fun startTimePicker () {
 
         // Инициализируем дата пикер и передаем туда 3 переменные для записи в них
-        datePickerDialog = DatePickerDialog(this, this, year, month, day)
-        datePickerDialog?.datePicker?.minDate = System.currentTimeMillis() - 1000
-        datePickerDialog?.setCanceledOnTouchOutside(false)
+        timePickerDialog = TimePickerDialog(this, this, hours, minutes,true)
+
+        // Этот метод держим выключенным(некорректно раньше работал) Отключал закрытие пикера
+        timePickerDialog?.setCanceledOnTouchOutside(false)
 
         // Создаем кнопки подтверждения и отказа
-        datePickerDialog?.setOnShowListener(DialogInterface.OnShowListener {
-            datePickerDialog?.getButton(Dialog.BUTTON_NEGATIVE)?.setTextColor(Color.WHITE)
-            datePickerDialog?.getButton(Dialog.BUTTON_POSITIVE)?.setTextColor(Color.WHITE)
+        // Здесь же можно задать стили для ПИКЕРА
+        timePickerDialog?.setOnShowListener(DialogInterface.OnShowListener {
+            timePickerDialog?.getButton(Dialog.BUTTON_NEGATIVE)?.setTextColor(Color.WHITE)
+            timePickerDialog?.getButton(Dialog.BUTTON_POSITIVE)?.setTextColor(Color.WHITE)
         })
 
-        // Запускает Дата пикер
-        datePickerDialog?.show()
+        // Запускает Тайм пикер
+        timePickerDialog?.show()
 
     }
 
-    // В этом методе к месяцу добавляем 1, ибо с массива месяца берутся начиная с нуля
+    // Получаем время выбранное на часах и записываем в текст вью
     @SuppressLint("SetTextI18n")
-    override fun onDateSet(datePicker: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
-        val saveMonth=month + 1
-        // При нажатии ПОдтвердить - записываем в текст вью ...
-        binding?.selected?.text = "$dayOfMonth.$saveMonth.$year"
+    override fun onTimeSet(timePicker: TimePicker?, hours: Int, minutes: Int) {
+        binding?.selected?.text = "$hours:$minutes"
     }
 }
